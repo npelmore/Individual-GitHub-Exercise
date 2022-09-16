15806e76cb2fa51bbaa10a5647ce4dfe91e5d35b
Nathan Pelmore <nate.b.pelmore@gmail.com>
2022-09-16 13:55:02 -0700

    preparing for feature 2

--- githubExercise/src/githubExercise/Main.java
+++ githubExercise/src/githubExercise/Main.java
@@ -6,20 +6,31 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-	
-		System.out.println("Please enter an integer:");
 		
-		int num1 = input.nextInt();
+		System.out.println("For comparing integers, please enter 1. For comparing strings, please enter 2.");
 		
-		System.out.println("Please enter another integer:");
+		int answer = input.nextInt();
 		
-		int num2 = input.nextInt();
+		if(answer == 1) {
+			System.out.println("Please enter an integer:");
+			
+			int num1 = input.nextInt();
+			
+			System.out.println("Please enter another integer:");
+			
+			int num2 = input.nextInt();
+			
+			int result = intCompare(num1,num2);
+			
+			if (result==1) System.out.println(num1+ " is greater than " + num2);
+			else if (result==2) System.out.println(num2 + " is greater than " + num1);
+			else System.out.println("The integers are equal");
+		}
+		else if(answer==2) {
+			
+		}
+		else System.out.println("You have entered neither 1 nor 2. Please restart to try again.");
 		
-		int result = intCompare(num1,num2);
-		
-		if (result==1) System.out.println(num1+ " is greater than " + num2);
-		else if (result==2) System.out.println(num2 + " is greater than " + num1);
-		else System.out.println("The integers are equal");
 	
 	}
 	
