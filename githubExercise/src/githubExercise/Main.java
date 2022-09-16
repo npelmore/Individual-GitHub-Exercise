3913af06bc3b3c3d85ecc78669572ef2ff48e06b
Nathan Pelmore <nate.b.pelmore@gmail.com>
2022-09-16 13:17:44 -0700

    started feature one

--- githubExercise/src/githubExercise/Main.java
+++ githubExercise/src/githubExercise/Main.java
@@ -1,10 +1,21 @@
 package githubExercise;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+	
+		System.out.println("Please enter an integer:");
+		
+		int num1 = input.nextInt();
+		
+		
+		System.out.println("Please enter another integer:");
+		
 	}
 
 }
